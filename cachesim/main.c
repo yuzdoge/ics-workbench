78ed770927a69f24286c85c52737f58233909a4d
tracer-nju <tracer@nju.edu.cn>
2023-01-31 22:10:03 +0800

    Static hostname: yuzdoge        Icon name: computer-container          Chassis: container       Machine ID: 03303304a27746d08dde1e33fd41d2ce          Boot ID: bf5cf255f3274be0994fdca68a0e2ef1   Virtualization: wsl Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.10.102.1-microsoft-standard-WSL2     Architecture: x86-64  22:10:03 up 1 day, 11:20,  0 users,  load average: 0.00, 0.00, 0.00

--- cachesim/main.c
+++ cachesim/main.c
@@ -58,7 +58,7 @@ static void random_trace(void) {
   for (i = 0; i < 100000; i ++) {
     t.t.len = choose_len[ choose(sizeof(choose_len) / sizeof(choose_len[0])) ] ;
     t.t.addr = choose(MEM_SIZE) & ~(t.t.len - 1);
-    t.t.is_write = choose(2);
+    t.t.is_write = choose(1);
     if (t.t.is_write) t.data = rand();
 
     trace_exec(&t, true);
