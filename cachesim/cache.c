e67bec95eeec63880a55e681ff54d10ebc6c59ba
tracer-nju <tracer@nju.edu.cn>
2023-02-02 22:25:40 +0800

    Static hostname: yuzdoge        Icon name: computer-container          Chassis: container       Machine ID: 03303304a27746d08dde1e33fd41d2ce          Boot ID: bf5cf255f3274be0994fdca68a0e2ef1   Virtualization: wsl Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.10.102.1-microsoft-standard-WSL2     Architecture: x86-64  22:25:40 up 3 days, 11:35,  0 users,  load average: 0.03, 0.02, 0.00

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -26,7 +26,8 @@ struct cache_line {
 static struct cache_line **cache;
 static int nway, nset; 
 static int index_width, tag_width;
-static uint32_t hit, miss, tot;
+static uint32_t hit, miss
+uint32_t tot;
 
 #define test_bit(stat, flag) (((stat) & (flag)) != 0)
 #define set_stat(stat, flag) ((stat) | (flag)) 
